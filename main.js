d5527c4a5b782fabcad64ca1229e4734e1cd369b
yivanova <yuliya.maslenka@gmail.com>
2024-05-24 14:08:36 +1000

    add proper discount for products

--- main.js
+++ main.js
@@ -9,29 +9,27 @@ const fetchData = async () => {
     }
 }
 
-// 5% Discount Counter
-const discountCounter = (price) => {
-    return price - (price * 0.05).toFixed(2);
-}
-
 // Create Product Cards
 let products = document.getElementById('products');
 const productCards = fetchData();
 
 productCards.then(data => {
     let cards = data.map(card => {
-        let discountedPrice = discountCounter(card.price);
-        return `<div class="col-md-3 col-sm-6 card-wrapper">
+            return `<div class="col-md-3 col-sm-6 card-wrapper">
                     <div class="card-item">
-                    <span class="discount">-5%</span>
+                        ${card.discount ? `<span class="discount">${card.discountPercentage}</span>` : ""}
                         <div class="card-image">
                             <img src="${card.image}" alt="${card.image}">
                         </div>
                         <div class="card-item-body">
                             <h6><strong>${card.name}</strong> <br> ${card.quantity}</h6>
                             <div class="card-item-price">
-                                <span>$ ${discountedPrice.toFixed(2)}</span>
-                                <span>Was $ ${card.price}</span>
+                                ${card.discount 
+                                    ? `<span style="color: coral; font-size: 1.5em; font-weight: 700;">$ ${card.priceWithDiscount}</span>` 
+                                    : ""}
+                                ${card.discount 
+                                    ? `<span style="text-decoration: line-through;">Was $ ${card.price}</span>` 
+                                    : `<span style="font-size: 1.5em;">$ ${card.price}</span>`}
                             </div>
                             <div class="card-brand">
                                 <p>${card.brand}</p>
